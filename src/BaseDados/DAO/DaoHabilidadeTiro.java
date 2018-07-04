b57309cd851a3b5735a65214b1e9b390df0a55c6
Voull <gabrielviana57@gmail.com>
2018-07-03 21:27:20 -0300

    Implementação Parcial dos DAOs
    - DaoItemJdbc implementado
    - DaoConsumivelJdbc implementado
    - DaoArmaJdbc parcialmente implementado (Metodo de listagem contém desafios imprevistos)
    
    -Alteração das Tabelas do Banco de Dados (Commit Pendente)

---
+++ src/BaseDados/DAO/DaoHabilidadeTiro.java
@@ -0,0 +1,8 @@
+package BaseDados.DAO;
+
+/**
+ * Created by Gabriel on 30/06/2018.
+ */
+public interface DaoHabilidadeTiro {
+
+}
--- src/BaseDados/DaoJDBC/DaoArmaJdbc.java
+++ src/BaseDados/DaoJDBC/DaoArmaJdbc.java
@@ -1,23 +1,43 @@
 package BaseDados.DaoJDBC;
 
 import BaseDados.BaseDadosException;
-import BaseDados.DAO.DaoArma;
+import BaseDados.DaoFactory_Teste.*;
 import DTO.Itens.Arma;
+import DTO.Itens.FolhaDano;
+import DTO.Itens.Item;
+import DTO.Personagens.SetsDeHabilidade.HabilidadesLuta;
+import DTO.Personagens.SetsDeHabilidade.HabilidadesTiro;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class DaoArmaJdbc extends BancoDadosJdbc implements DaoArma{
+import static java.util.Collections.addAll;
 
-    public DaoArmaJdbc()throws BaseDadosException{
+public class DaoArmaJdbc extends BancoDadosJdbc implements DaoArma {
+
+    DaoItem daoItem;
+    DaoFolhaDano daoFolhaDano;
+    DaoHabilidadesTiro daoHabilidadesTiro;
+    DaoHabilidadesLuta daoHabilidadesLuta;
+
+    public DaoArmaJdbc(DaoItem daoItem, DaoFolhaDano daoFolhaDano, DaoHabilidadesLuta daoHabilidadesLuta, DaoHabilidadesTiro daoHabilidadesTiro)throws Exception{
         super();
+        this.daoItem = daoItem;
+        this.daoFolhaDano = daoFolhaDano;
+        this.daoHabilidadesTiro = daoHabilidadesTiro;
+        this.daoHabilidadesLuta = daoHabilidadesLuta;
     }
 
     @Override
-    public Arma BuscaArma(int codigo) throws BaseDadosException {
+    public Arma Busca(int codigo) throws BaseDadosException {
+        Item item = daoItem.Busca(codigo);
+        HabilidadesTiro habilidadesTiro = daoHabilidadesTiro.Busca(codigo);
+        HabilidadesLuta habilidadesLuta = daoHabilidadesLuta.Busca(codigo);
+        FolhaDano folhaDano = daoFolhaDano.Busca(codigo);
         abreConexao();
         try {
-            preparaComandoSQL("SELECT * FROM arma LEFT JOIN item ON arma.id_item = item.id_item WHERE arma.id_item = ?");
+            preparaComandoSQL("SELECT * FROM arma WHERE id_item = ?");
             ps.setInt(1, codigo);
             ps.executeQuery();
         }
@@ -28,32 +48,152 @@ public class DaoArmaJdbc extends BancoDadosJdbc implements DaoArma{
 
         try{
             rs.next();
-            Arma item = new Arma(rs.getInt("id_item"), rs.getString("nome_item"), rs.getString("descricao_item"), );
+            Arma arma = new Arma(item.getId(), item.getNome(), item.getDescricao(), habilidadesTiro, habilidadesLuta,
+                    folhaDano, rs.getShort("usos_round"), rs.getShort("tamanho_pente"),
+                    rs.getShort("mal_funcionamento"));
             fechaConexao();
-            return item;
+            return arma;
         }catch (SQLException e ){
             fechaConexao();
-            throw new BaseDadosException("Item nao encontrado");
+            throw new BaseDadosException("Arma nao encontrado");
         }
     }
 
     @Override
-    public void EscreveArma(Arma arma) throws BaseDadosException {
+    public void Insere(Arma arma) throws BaseDadosException {
+        daoItem.Insere(arma);
+        daoHabilidadesLuta.Insere(arma.getHabilidadesUsadasLuta());
+        daoHabilidadesTiro.Insere(arma.getHabilidadesUsadasTiro());
+        daoFolhaDano.Insere(arma.getDano());
+
+        abreConexao();
+        try{
+            preparaComandoSQL("INSERT INTO arma (usos_round, tamanho_pente, mal_funcionamento, nome) VALUES (?, ?, ?) SELECT nome FROM item WHERE item.item_nome = ?");
+            ps.setShort(1, arma.getUsosPorRound());
+            ps.setShort(2, arma.getTamanhoDoPente());
+            ps.setShort(3, arma.getMalFuncionamento());
+            ps.setString(4, arma.getNome());
+            ps.execute();
+        }catch (SQLException e){
+            throw new BaseDadosException("Nao foi possivel adicionar Arma");
+        }
+    }
+
+    @Override
+    public void Altera(Arma arma) throws BaseDadosException {
+        daoItem.Altera(arma);
+        daoHabilidadesTiro.Altera(arma.getHabilidadesUsadasTiro());
+        daoHabilidadesLuta.Altera(arma.getHabilidadesUsadasLuta());
+        daoFolhaDano.Altera(arma.getDano());
+
+        abreConexao();
+        try{
+            preparaComandoSQL("UPDATE arma SET usos_round = ?, tamanho_pente = ?, mal_funcionamento = ? WHERE id_item = ?");
+            ps.setShort(1, arma.getUsosPorRound());
+            ps.setShort(2, arma.getTamanhoDoPente());
+            ps.setShort(3, arma.getMalFuncionamento());
+            ps.setInt(4, arma.getId());
+            ps.execute();
+        }catch (SQLException e){
+            throw new BaseDadosException("Nao foi possivel modificar Arma");
+        }
+
+    }
+
+    public void Remove(int codigo) throws BaseDadosException{
+        daoItem.Remove(codigo);
+        daoHabilidadesTiro.Remove(codigo);
+        daoHabilidadesLuta.Remove(codigo);
+        daoFolhaDano.Remove(codigo);
+
+        abreConexao();
+        try{
+            preparaComandoSQL("DELETE FROM arma WHERE id.item = ?");
+            ps.setInt(1, codigo);
+            ps.execute();
+        }catch(SQLException e){
+            throw new BaseDadosException("Não foi possível remover a Arma");
+        }
 
     }
 
     @Override
-    public void AlteraArma(Arma arma) throws BaseDadosException {
+    public List<Arma> Lista() throws BaseDadosException {
+        abreConexao();
+        try {
+            preparaComandoSQL("SELECT * FROM arma LEFT JOIN item ON arma.id_item = item.id_item WHERE item.tipo_item = Arma");
+            ps.executeQuery();
+        }
+        catch (SQLException e){
+            fechaConexao();
+            throw new BaseDadosException("Erro no acesso");
+        }
+        ArrayList<Arma> armas = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                HabilidadesTiro habilidadesTiro = daoHabilidadesTiro.Busca(rs.getInt("id_item"));
+                HabilidadesLuta habilidadesLuta = daoHabilidadesLuta.Busca(rs.getInt("id_item"));
+                FolhaDano folhaDano = daoFolhaDano.Busca(rs.getInt("id_item"));
 
+                Arma arma = new Arma(rs.getInt("id_item"), rs.getString("nome_item"),
+                        rs.getString("descricao_item"), habilidadesTiro, habilidadesLuta, folhaDano,
+                        rs.getShort("usos_round"), rs.getShort("tamanho_pente"),
+                        rs.getShort("mal_funcionamento"));
+                armas.add(arma);
+            }
+            return armas;
+        }
+        catch(SQLException e){
+            fechaConexao();
+            throw new BaseDadosException("Erro no aceeso ao BD");
+        }
+        /*
+        List<HabilidadesTiro> listaHabilidadesTiro = daoHabilidadesTiro.Lista();
+        List<HabilidadesLuta> listaHabilidadesLuta = daoHabilidadesLuta.Lista();
+        List<FolhaDano> listaFolhaDano = daoFolhaDano.Lista();
+
+        abreConexao();
+        try {
+            preparaComandoSQL("SELECT * FROM arma LEFT JOIN item ON arma.id_item = item.id_item WHERE item.tipo_item = Arma");
+            ps.executeQuery();
+        }
+        catch (SQLException e){
+            fechaConexao();
+            throw new BaseDadosException("Erro no acesso");
+        }
+        ArrayList<Arma> armas = new ArrayList<>();
+        try{
+            while(rs.next()){
+                for(HabilidadesTiro tiro : listaHabilidadesTiro){
+                    if(tiro.getId() == rs.getInt("id_item")){
+                        for(HabilidadesLuta luta : listaHabilidadesLuta){
+                            if(luta.getId() == rs.getInt("id_item")){
+                                for(FolhaDano folhaDano : listaFolhaDano){
+                                    if(folhaDano.getId() == rs.getInt("id_item")){
+                                        Arma arma = new Arma(rs.getInt("id_item"), rs.getString("nome_arma"), rs.getString("descricao_arma"), tiro, luta, folhaDano, rs.getShort("usos_round"), rs.getShort("tamanho_pente"), rs.getShort("mal_funcionamento"));
+                                        armas.add(arma);
+                                        listaFolhaDano.remove(folhaDano);
+                                    }
+
+                                }
+                               listaHabilidadesLuta.remove(luta);
+                            }
+
+                        }
+                    }
+                }
+            }
+            fechaConexao();
+            return armas;
+        }catch (SQLException e){
+            fechaConexao();
+            throw new BaseDadosException("Erro no acesso");
+        }
+        */
     }
 
     @Override
-    public List<Arma> ListaArma() throws BaseDadosException {
-        return null;
-    }
+    public List<Arma> ListaDoPersonagem(int codigo) throws BaseDadosException {
 
-    @Override
-    public List<Arma> ListaArmaPersonagem(int codigo) throws BaseDadosException {
-        return null;
     }
 }
